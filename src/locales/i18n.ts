ecf05383088c4e284cc0069a13d24fd4c092333e
heppokofrontend <heppokofrontend@gmail.com>
2021-09-21 00:40:12 +0900

    :zap: 多言語対応

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -52,7 +52,7 @@ i18n
   .init({
     resources,
     fallbackLng: ['en', 'ja'],
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: window.navigator.languages[0] === 'ja' ? 'ja' : 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
