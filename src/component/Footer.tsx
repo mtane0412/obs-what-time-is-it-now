10826e16ea2e2a8e57b3e6c6ab18c7372e24beb2
heppokofrontend <heppokofrontend@gmail.com>
2021-09-20 23:10:45 +0900

    :zap: Fix URL

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <p>
-        <a href="https://github.com/KawarabeEcma/obs-what-time-is-it-now">Source on GitHub</a>
+        <a href="https://github.com/heppokofrontend/obs-what-time-is-it-now">Source on GitHub</a>
       </p>
     </footer>
   );
